a835eb3232f7f2eadac1a17ba2799e92ccab95a8
fukushima-hideto <Fukushima.Hideto@technopro.com>
2025-03-21 10:52:44 +0900

    解き直し完了

--- app/src/main/java/com/recipeapp/datahandler/CSVDataHandler.java
+++ app/src/main/java/com/recipeapp/datahandler/CSVDataHandler.java
@@ -1,7 +1,13 @@
 package com.recipeapp.datahandler;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import com.recipeapp.model.Ingredient;
 import com.recipeapp.model.Recipe;
 
 public class CSVDataHandler implements DataHandler{
@@ -15,18 +21,66 @@ public class CSVDataHandler implements DataHandler{
         this.filePath = filePath;
     }
 
+    @Override
     public String getMode(){
         return "CSV";
     }
 
+    @Override
     public ArrayList<Recipe> readData() throws IOException{
-        return null;
+        ArrayList<Recipe> recipes = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath));){
+            String line;
+            String name = "";
+            //1行ごと受け取る
+            while((line = reader.readLine()) != null ){
+                ArrayList<Ingredient> ingredients = new ArrayList<>();
+                //,で区切りはじめはnameに残りはingredientsに追加する
+                String[] pairs = line.split(",");
+                for(int i = 0; i <pairs.length; i++){
+                    if(i == 0){
+                        name = pairs[i];
+                    } else {
+                        ingredients.add(new Ingredient(pairs[i]));
+                    }
+                }
+                //ArrayList<Recipe>に代入する
+                recipes.add(new Recipe(name, ingredients));
+            }
+        }catch(IOException e){
+            System.out.println("Error reading file:" + e.getMessage());
+        }
+
+        return recipes;
     }
 
+    @Override
     public void writeData(Recipe recipe) throws IOException{
-
+        String text = recipe.getName();
+        String ingredient = "";
+        for(int i = 0; i < recipe.getIngredients().size(); i++){
+            ingredient += "," + recipe.getIngredients().get(i).getName();
+        }
+        text += ingredient;
+        BufferedWriter writer = null;
+        try {
+            writer =new BufferedWriter(new FileWriter(filePath, true));
+            writer .write(text);
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("入力に誤りがありました。");
+        }finally{
+            if(writer != null){
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
+    @Override
     public ArrayList<Recipe> searchData(String keyword) throws IOException{
         return null;
     }
--- app/src/main/java/com/recipeapp/ui/RecipeUI.java
+++ app/src/main/java/com/recipeapp/ui/RecipeUI.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
+
+import com.recipeapp.datahandler.DataHandler;
+import com.recipeapp.model.Ingredient;
+import com.recipeapp.model.Recipe;
 
 public class RecipeUI {
     private BufferedReader reader;
@@ -33,8 +36,10 @@ public class RecipeUI {
 
                 switch (choice) {
                     case "1":
+                        displayRecipes();
                         break;
                     case "2":
+                        addNewRecipe();
                         break;
                     case "3":
                         break;
@@ -49,5 +54,73 @@ public class RecipeUI {
                 System.out.println("Error reading input from user: " + e.getMessage());
             }
         }
+    }
+
+    private void displayRecipes(){
+        try {
+            ArrayList<Recipe> recipes = dataHandler.readData();
+            if(recipes.isEmpty()){
+                System.out.println();
+                System.out.println("No recipes available.");
+                return;
+            }
+
+            System.out.println("Recipes:");
+            System.out.println("-----------------------------------");
+            for(Recipe recipe : recipes){
+                //nameを受け取る
+                System.out.println("Recipe Name:" + recipe.getName());
+                //ingredientを受け取る
+                String text = "";
+                ArrayList<Ingredient> ingredients = recipe.getIngredients();
+                for(int i = 0; i < ingredients.size(); i++){
+                    text += ingredients.get(i).getName();
+                    if(i < ingredients.size() - 1){
+                        text += ",";
+                    }
+                }
+                System.out.println("Main Ingredients:" + text);
+                System.out.println("-----------------------------------");
+    
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file:" + e.getMessage());
+        }
+    }
+
+    private void addNewRecipe(){
+        try {
+            //レシピ名入力
+            System.out.println("Adding a new recipe.");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter recipe name:");
+            String RecipeName = reader.readLine();
+
+            //材料名入力
+            ArrayList<Ingredient> ingredients = new ArrayList<>();
+            System.out.println("Enter ingredients (type 'done' when finished):");
+            while(true){
+                System.out.print("Ingredient:");
+                String ingredient = reader.readLine();
+
+                if(ingredient.equals("done")){
+                    System.out.println("Recipe added successfully.");
+                    break;
+                }
+                ingredients.add(new Ingredient(ingredient));
+            }
+
+            //インスタンス化
+            Recipe recipe = new Recipe(RecipeName, ingredients);
+            //writeData()メソッド使用
+            dataHandler.writeData(recipe);
+
+        } catch (IOException e) {
+            System.out.println("Failed to add new recipe:" + e.getMessage());
+        }
+
+
+
+
     }
 }
